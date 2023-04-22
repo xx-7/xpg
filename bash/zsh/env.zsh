836f146a08d8dd549eb00ea84b1653039a5ef81c
xx-7 <xx-7@live.com>
2023-04-22 11:09:13 +0800

    flutter config

--- bash/zsh/env.zsh
+++ bash/zsh/env.zsh
@@ -10,6 +10,7 @@ export LC_ALL=en_US.utf8
 
 # flutter
 export PATH=$PATH:/optd/opt/flutter/bin
-export FLUTTER_STORAGE_BASE_URL=https://mirror.sjtu.edu.cn/
-export PUB_HOSTED_URL=https://mirror.sjtu.edu.cn/dart-pub/
+export FLUTTER_STORAGE_BASE_URL=https://storage.flutter-io.cn/
+export PUB_HOSTED_URL=https://pub.flutter-io.cn
+export FLUTTER_GIT_URL=https://mirrors.tuna.tsinghua.edu.cn/git/flutter-sdk.git
 export CHROME_EXECUTABLE=/opt/google/chrome/chrome
