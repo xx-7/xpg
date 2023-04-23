2a1cb809fac0e3ea68a6becf260ec8fb69759b23
xx-7 <xx-7@live.com>
2023-04-23 15:33:40 +0800

    zsh git

--- bash/zsh/git.zsh
+++ bash/zsh/git.zsh
@@ -1,4 +1,20 @@
 
+function git_current_branch() {
+  local ref
+  ref=$(git symbolic-ref --quiet HEAD 2> /dev/null)
+  local ret=$?
+  if [[ $ret != 0 ]]; then
+    [[ $ret == 128 ]] && return  # no git repo.
+    ref=$(git rev-parse --short HEAD 2> /dev/null) || return
+  fi
+  echo ${ref#refs/heads/}
+}
+
+gam () {
+    git add .
+    git commit -m "$1"
+    git push origin $(git_current_branch)
+}
 
 alias gita='git add .'
 alias gitc='git commit -m'
@@ -7,10 +23,4 @@ alias gitp='git push origin'
 alias gitcl='git config --list'
 alias gitpc='git config --global --unset http.proxy && git config --global --unset https.proxy'
 alias gitps='git config --global http.proxy "socks5://127.0.0.1:11019" && git config --global https.proxy "socks5://127.0.0.1:11019"'
-alias gitfr='git fetch origin && git reset --hard origin $(git_current_branch) '
-
-gam () {
-    git add .
-    git commit -m "$1"
-    git push origin $(git_current_branch)
-}
+alias gitfr='git fetch origin && git reset --hard origin $(git_current_branch)'
